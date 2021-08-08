77e6e3bba9c3eae63854ea69675d4721befeb570
Takieddin <t.fellag@esi-sba.dz>
2021-08-08 04:17:41 +0100

    stock models

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=200)
+
+class Brand(models.Model):
+    name = models.CharField(max_length=200)    
+    category = models.ForeignKey(Category, related_name='brands', on_delete=models.CASCADE)
+class Supplier(models.Model):
+    name = models.CharField(max_length=200)    
+    phone = models.IntegerField(unique=True)
+class Soug(models.Model):
+    name = models.CharField(max_length=200)
+    note = models.TextField(blank=True)
+    date =models.DateField()
+    total=models.IntegerField()
+    paied =models.IntegerField()
+    
+class Stock(models.Model):
+    name = models.CharField(max_length=200)
+    note = models.TextField(blank=True)
+    quantity  = models.IntegerField()
+    date =models.DateField()
+    item_buying_price = models.IntegerField()
+    item_sale_price = models.IntegerField()
+    instock=models.IntegerField()
+    supplier = models.ForeignKey(Supplier, related_name='stocks', on_delete=models.CASCADE,null=False,default=1)
+    soug = models.ForeignKey(Soug, related_name='stocks', on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, related_name='stocks', on_delete=models.CASCADE,null=False,default=1)
