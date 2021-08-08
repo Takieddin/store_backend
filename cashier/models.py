8b8546356b0fe45117bc0a6adac0fd399078db75
Takieddin <t.fellag@esi-sba.dz>
2021-08-08 10:21:50 +0100

    cashier models

--- cashier/models.py
+++ cashier/models.py
@@ -1,3 +1,46 @@
-from django.db import models
 
-# Create your models here.
+from sales.models import Basket, Client
+from django.db import models
+from stock.models import Brand, Soug, Stock
+
+
+class Cashier(models.Model):
+    amount = models.IntegerField()
+    date = models.DateTimeField()
+
+
+class Log(models.Model):
+    date = models.DateTimeField()
+    profit = models.IntegerField()
+    expance = models.IntegerField()
+    credit = models.IntegerField()
+    brand = models.ForeignKey(
+        Brand, related_name='logs', on_delete=models.CASCADE)
+
+
+class Expance(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    details = models.TextField(blank=True)
+    quantity = models.IntegerField()
+    date = models.DateTimeField()
+    amount = models.IntegerField()
+    soug = models.ForeignKey(
+        Soug, related_name='expances', on_delete=models.CASCADE)
+
+
+class Diposite(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    details = models.TextField(blank=True)
+    quantity = models.IntegerField()
+    date = models.DateTimeField()
+    amount = models.IntegerField()
+
+
+class Restore(models.Model):
+    client = models.ForeignKey(
+        Client, related_name='restores', on_delete=models.CASCADE, null=False, default=1)
+    total = models.IntegerField()
+    quantity = models.IntegerField()
+    date = models.DateTimeField()
+    basket = models.ForeignKey(
+        Basket, related_name='restores', on_delete=models.CASCADE, null=False, default=1)
