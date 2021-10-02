23cf743a96d2bf80457219fc43ed4dcb2ae47bd7
Takieddin <t.fellag@esi-sba.dz>
2021-10-03 00:08:19 +0100

    fine v

--- InventoryManager/settings.py
+++ InventoryManager/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 import django_heroku
-django_heroku.settings(locals())
 
 
 from pathlib import Path
@@ -147,3 +146,5 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
